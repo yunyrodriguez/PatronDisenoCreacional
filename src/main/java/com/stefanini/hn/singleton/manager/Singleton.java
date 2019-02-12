80d06d3f517172c45346ea8bc1c46c66a85b3f80
yunyrodriguez <yunirene@hotmail.com>
2019-02-12 09:59:53 -0600

    adicion de test para Singleton

--- src/main/java/com/stefanini/hn/singleton/manager/Singleton.java
+++ src/main/java/com/stefanini/hn/singleton/manager/Singleton.java
@@ -1,15 +1,43 @@
 package com.stefanini.hn.singleton.manager;
 
 public class Singleton {
+
+	private String url;
+	private String baseDatos;
 	private static Singleton instance = null;
+
+	public static Singleton getInstance(String url, String baseDatos) {
+
+		if (instance == null) {
+
+			instance = new Singleton(url, baseDatos);
+		}
+		return instance;
+	}
+
+	private Singleton(String url, String baseDatos) {
+
+		this.url = url;
+		this.baseDatos = baseDatos;
+
+	}
 
 	private Singleton() {
 	}
 
-	public static Singleton GetInstance() {
-		if (instance == null)
-			instance = new Singleton();
+	public String getUrl() {
+		return url;
+	}
 
-		return instance;
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getBaseDatos() {
+		return baseDatos;
+	}
+
+	public void setBaseDatos(String baseDatos) {
+		this.baseDatos = baseDatos;
 	}
 }
